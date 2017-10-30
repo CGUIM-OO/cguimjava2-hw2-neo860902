a7d54193b96af7fd00dc2352780ba17e9b877186
neo860902 <32242640+neo860902@users.noreply.github.com>
2017-10-30 18:30:35 +0800

    Update HW2.java

--- HW2.java
+++ HW2.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author TODO: please add student ID and name here
+ * @author TODO:B0544142 邱楷翔
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -64,54 +64,82 @@ public class HW2 {
 
 }
 /**
- * Description: TODO: please add description here
+ * Description: 第一個for迴圈部分是要給幾副牌的迴圈 第二個是有幾種花色的迴圈 第三個是牌的大小的迴圈 
+ * 再下面的部分是把ArrayList裡面的值印出來
  */
-class Deck{
+class Deck {
 	private ArrayList<Card> cards;
-	//TODO: Please implement the constructor (30 points)
-	public Deck(int nDeck){
-		cards=new ArrayList<Card>();
-		//1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
-		//Hint: Use new Card(x,y) and 3 for loops to add card into deck
-		//Sample code start
-		//Card card=new Card(1,1); ->means new card as clubs ace
-		//cards.add(card);
-		//Sample code end
 
-	}	
-	//TODO: Please implement the method to print all cards on screen (10 points)
-	public void printDeck(){
-		//Hint: print all items in ArrayList<Card> cards, 
-		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+	// TODO: Please implement the constructor (30 points)
+	public Deck(int nDeck) {
+		cards = new ArrayList<Card>();
+		// 1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
+		// Hint: Use new Card(x,y) and 3 for loops to add card into deck
+		// Sample code start
+		// Card card=new Card(1,1); ->means new card as clubs ace
+		// cards.add(card);
+		// Sample code end
+		for (int n = 1; n <= nDeck; n++) {
+			for (int s = 1; s <= 4; s++) {
+				for (int r = 1; r <= 13; r++) {
+					Card card = new Card(s, r);
+					cards.add(card);
+				}
+			}
+		}
 	}
-	public ArrayList<Card> getAllCards(){
+
+	// TODO: Please implement the method to print all cards on screen (10
+	// points)
+	public void printDeck() {
+		// Hint: print all items in ArrayList<Card> cards,
+		// TODO: please implement and reuse printCard method in Card class (5
+		// points)
+		for (int n = 0; n < cards.size(); n++) {
+			cards.get(n).printCard();
+		}
+	}
+
+	public ArrayList<Card> getAllCards() {
 		return cards;
 	}
 }
+
 /**
- * Description: TODO: please add description here
+* Description:設定陣列Clubs=1, Diamonds=2, Hearts=3,Spades=4 1
+* 牌的數字1~13
  */
-class Card{
-	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
-	private int rank; //1~13
+class Card {
+	private int suit; // Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3,Spades=4
+	private String[] suitname = { "1", "2", "3", "4" };
+	private int rank; // 1~13
+	private String[] rankname = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" };
+
 	/**
-	 * @param s suit
-	 * @param r rank
+	 * @param s
+	 *            suit
+	 * @param r
+	 *            rank
 	 */
-	public Card(int s,int r){
-		suit=s;
-		rank=r;
-	}	
-	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
-	public void printCard(){
-		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
+	public Card(int s, int r) {
+		suit = s;
+		rank = r;
+	}
+
+	// TODO: 1. Please implement the printCard method (20 points, 10 for suit,
+	// 10 for rank)
+	public void printCard() {
+		System.out.println(suitname[suit - 1] + " " + rankname[rank - 1]);
+		// Hint: print (System.out.println) card as suit,rank, for example:
+		// print 1,1 as Clubs Ace
 
 	}
-	public int getSuit(){
+
+	public int getSuit() {
 		return suit;
 	}
-	public int getRank(){
+
+	public int getRank() {
 		return rank;
 	}
 }
